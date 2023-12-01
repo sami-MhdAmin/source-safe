e46110d69e1a783ff6171d1c8e92267e68c3ec75
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2023-12-01 23:04:47 +0100

    Add swagger

--- src/main/java/com/example/sourceSafeMaven/SourceSafeMavenApplication.java
+++ src/main/java/com/example/sourceSafeMaven/SourceSafeMavenApplication.java
@@ -6,7 +6,14 @@ import com.example.sourceSafeMaven.security.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @SpringBootApplication
 public class SourceSafeMavenApplication {
@@ -20,6 +27,33 @@ public class SourceSafeMavenApplication {
 
     @Autowired
     private PasswordEncoder passwordEncode;
+
+    @Configuration
+    public static class OpenApiConfig {
+
+        // Swagger URL: http://localhost:8090/swagger-ui/index.html
+        @Bean
+        public OpenAPI customOpenAPI() {
+            var securityKey = "Auth Token";
+            var securityScheme = new SecurityScheme();
+            securityScheme
+                    .name("bearerAuth")
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer");
+            return new OpenAPI()
+                    .components(
+                            new Components().addSecuritySchemes(securityKey, securityScheme)
+                    )
+                    .addSecurityItem(new SecurityRequirement().addList(securityKey))
+                    .info(
+                            new Info()
+                                    .title("SafeSource - API")
+                                    .version("1.0.0")
+                                    .description("Backend documentation for SafeSource")
+                    );
+        }
+    }
+
 /*
     @Bean
     CommandLineRunner commandLineRunner(UserRepository userRepository, GroupRepository groupRepository, FileRepository fileRepository) {
