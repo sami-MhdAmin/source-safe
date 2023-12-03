87ee609cabdc898b3e6292c3f8d9031ca1a45a09
Sami <sami.amin.6.2001@gmail.com>
2023-12-03 11:49:46 +0300

    refactor

--- src/main/java/com/example/sourceSafeMaven/controller/VersionController.java
+++ src/main/java/com/example/sourceSafeMaven/controller/VersionController.java
@@ -2,6 +2,7 @@ package com.example.sourceSafeMaven.controller;
 
 import com.example.sourceSafeMaven.dto.AddFileDto;
 import com.example.sourceSafeMaven.entities.TextFile;
+import com.example.sourceSafeMaven.entities.Version;
 import com.example.sourceSafeMaven.repository.TextFileRepository;
 import com.example.sourceSafeMaven.security.JwtService;
 import com.example.sourceSafeMaven.service.TextFileService;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -63,6 +65,12 @@ public class VersionController {
         byte[] versionBytes = versionService.getVersionBytes(versionId);
 
         Optional<TextFile> textFile = textFileRepository.findById(fileId);
+        /////////////////////////
+        List<Version> versionsList = textFile.get().getVersions();
+        Version versionItem = versionsList.get(Integer.MAX_VALUE);
+        Long versId = versionItem.getId();
+
+        /////////////////////////
         String fileName = textFile.get().getFileName();
 //        fileName.concat(".txt");
 //            System.out.println("file name is " + fileName);
